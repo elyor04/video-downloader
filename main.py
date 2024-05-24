e9190cf3514e5d22f2f535e2e003ab5e7dccae83
elyor04 <tuxtamuratovelyor@gmail.com>
2024-05-24 15:49:25 +0500

    Just commit

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ pyinstaller main.py --onefile --windowed --name="Video Downloader" --icon="icon.
 """
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from ui import DownloaderApp
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.png"))
     win = DownloaderApp()
     win.show()
     sys.exit(app.exec())
